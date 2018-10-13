4c3cc4f7f7ea9f381286990bd11d05586f77da3f
yang <yzxuan2534@gmail.com>
2018-10-14 01:13:29 +0800

    wk5finally

--- guestbook/models.py
+++ guestbook/models.py
@@ -4,7 +4,8 @@ from django.conf import settings
 class TextMessage(models.Model):
 	talker= models.CharField(max_length=30,blank=False)
 	message= models.CharField(max_length=30,blank=True)
+	date_time = models.DateTimeField()
 
 	def _str_(self):
-		return self.talker+""+self.message
+		return self.talker+""+self.message+""+self.date_time
 # Create your models here.
--- helloworld/views.py
+++ helloworld/views.py
@@ -4,15 +4,18 @@ from django.contrib import auth
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from guestbook.models import TextMessage
+import datetime
 
 def index(request):
-	t1= TextMessage.objects.create(talker="Nick",message="Hi,Yang!")
-	t2= TextMessage.objects.create(talker="Yang",message="Hi!Nick")
-	t3= TextMessage.objects.create(talker="Swai",message="Hey GUYS!")
-	msgs= TextMessage.objects.all()
-	return render(request, 'guestbookver1.html',locals())
+        if 'ok' in request.POST:
+                talker = request.POST['talker']
+                message = request.POST['message']
+                date_time=datetime.datetime.now()
+                TextMessage.objects.create(talker=talker,message=message,date_time=date_time)
+                msgs=TextMessage.objects.all()                        
+        msgs=TextMessage.objects.all()
+        return render(request, 'guestbookver1.html',locals())
 
 def yang(request):
         buttonlist=['red','blue','green','yellow']
         return render(request,'yang.html',{'buttonlist':buttonlist})
-
