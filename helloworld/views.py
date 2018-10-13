8466340e08da37bb469552311a855b6c3936659e
yang <yzxuan2534@gmail.com>
2018-10-14 02:13:27 +0800

    wk5 like finally

--- helloworld/views.py
+++ helloworld/views.py
@@ -4,13 +4,16 @@ from django.contrib import auth
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from guestbook.models import TextMessage
-import datetime
+import pytz,datetime
 
 def index(request):
         if 'ok' in request.POST:
                 talker = request.POST['talker']
                 message = request.POST['message']
+                #local=pytz.timezone('Asia/Taipei')
                 date_time=datetime.datetime.now()
+                #local_dt=local.localize(date_time1,is_dst=None)
+                #date_time=date_time1.replace(tzinfo=local)
                 TextMessage.objects.create(talker=talker,message=message,date_time=date_time)
                 msgs=TextMessage.objects.all()                        
         msgs=TextMessage.objects.all()
